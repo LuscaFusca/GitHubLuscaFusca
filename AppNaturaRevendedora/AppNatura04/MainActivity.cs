9425f0655e0faedfa8e3aa2a749eab93113e333e
LuscaFusca <lucasgomes_2006@hotmail.com>
2020-10-11 12:55:54 -0300

    App Natura Revendedora

---
+++ AppNaturaRevendedora/AppNatura04/MainActivity.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+using Android.Gms.Vision;
+using Android.Gms.Vision.Texts;
+using Android.Graphics;
+using Android.Support.V4.App;
+using Android;
+using Android.Util;
+using Android.Content.PM;
+using static Android.Gms.Vision.Detector;
+using Java.Lang;
+using Java.IO;
+using System.Text.RegularExpressions;
+using Android.Webkit;
+
+namespace AppNaturaRevendedora {
+    [Activity(Label = "@string/app_name", MainLauncher = true, Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity {
+        private Button btnComprar;
+        private Button btnCarrinho;
+        private Button btnAjuda;
+        private WebView webViewInicial;
+
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            btnComprar = FindViewById<Button>(Resource.Id.btnComprar);
+            btnCarrinho = FindViewById<Button>(Resource.Id.btnCarrinho);
+            webViewInicial = FindViewById<WebView>(Resource.Id.webViewInicial);
+
+            btnComprar.Click += BtnComprar_Click;
+            btnCarrinho.Click += BtnCarrinho_Click;
+
+            btnAjuda = FindViewById<Button>(Resource.Id.btnAjuda);
+            btnAjuda.Click += BtnAjuda_Click;
+
+            webViewInicial.SetWebViewClient(new ExtendWebViewClient());
+            webViewInicial.LoadUrl("https://www.natura.com.br/c/tudo-em-promocoes");
+
+            webViewInicial.Settings.DomStorageEnabled = true;
+
+            WebSettings webSettings = webViewInicial.Settings;
+            webSettings.JavaScriptEnabled = true;
+        }
+
+        internal class ExtendWebViewClient : WebViewClient {
+            public override bool ShouldOverrideUrlLoading(WebView view, string url) {
+                view.LoadUrl(url);
+                return true;
+            }
+        }
+
+        private void BtnCarrinho_Click(object sender, System.EventArgs e) {
+            StartActivity(typeof(ListaCarrinho));
+        }
+
+        private void BtnAjuda_Click(object sender, System.EventArgs e) {
+            StartActivity(typeof(chat));
+        }
+
+        private void BtnComprar_Click(object sender, System.EventArgs e) {
+            StartActivity(typeof(ColetaCodigo));
+        }
+    }
+}
---
+++ AppNaturaRevendedora/AppNatura04/chat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+
+namespace AppNaturaRevendedora {
+    [Activity(Label = "chat")]
+    public class chat : Activity {
+
+        WebView webView;
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.chat);
+
+            webView = FindViewById<WebView>(Resource.Id.webView);
+            webView.SetWebViewClient(new ExtendWebViewClient());
+            webView.LoadUrl("https://mdh-chat.metasix.solutions/livechat?mode=popout");
+            
+            webView.Settings.DomStorageEnabled = true;
+            
+            WebSettings webSettings = webView.Settings;
+            webSettings.JavaScriptEnabled = true;
+        }
+    }
+
+    internal class ExtendWebViewClient : WebViewClient {
+        public override bool ShouldOverrideUrlLoading(WebView view, string url) {
+            view.LoadUrl(url);
+            return true;
+        }
+    }
+}
